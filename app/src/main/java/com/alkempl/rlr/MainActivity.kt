cf9503192e308a6e27762baaa4c96fb650d1506d
alex <alkempled@gmail.com>
2021-09-13 02:14:37 +0300

    new beautiful layout Locus -> Google

--- app/src/main/java/com/alkempl/rlr/MainActivity.kt
+++ app/src/main/java/com/alkempl/rlr/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.alkempl.rlr
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,9 +9,10 @@ import android.text.Editable
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import com.alkempl.rlr.databinding.ActivityMainBinding
 import com.birjuvachhani.locus.Locus
-import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.*
 import java.text.DateFormat.getDateTimeInstance
 import java.text.SimpleDateFormat
 import java.util.*
@@ -18,18 +21,34 @@ class MainActivity : AppCompatActivity() {
     var locationEnabled = false
     private lateinit var binding: ActivityMainBinding
 
+    // declare a global variable FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // in onCreate() initialize FusedLocationProviderClient
+
+
+    // globally declare LocationRequest
+    private lateinit var locationRequest: LocationRequest
+
+    // globally declare LocationCallback
+    private lateinit var locationCallback: LocationCallback
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Locus.configure {
-            forceBackgroundUpdates = true // default: false
-            enableBackgroundUpdates = true // default: false
-            request {
-                fastestInterval = 0
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                interval = 0
-                maxWaitTime = 2
-            }
-        }
+//        Locus.configure {
+//            forceBackgroundUpdates = true // default: false
+//            enableBackgroundUpdates = true // default: false
+//            request {
+//                fastestInterval = 0
+//                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//                interval = 0
+//                maxWaitTime = 2
+//            }
+//        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+
+        getLocationUpdates()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
@@ -39,48 +58,48 @@ class MainActivity : AppCompatActivity() {
 
         binding.forceButton.setOnClickListener {
 //            if (locationEnabled) {
-                Locus.getCurrentLocation(this) { result ->
-                    result.location?.let { /* Received location update */
-                        binding.editTextTextMultiLine.setText(
-                            generateLocationOutput(it)
-                        )
-                    }
-                    result.error?.let { /* Received error! */ }
+            Locus.getCurrentLocation(this) { result ->
+                result.location?.let { /* Received location update */
+                        setLocationOutput(it)
                 }
+                result.error?.let { /* Received error! */ }
+            }
 //            }
         }
         binding.switchButton.setOnClickListener {
             if (locationEnabled) {
                 locationEnabled = false;
                 binding.switchButton.text = getString(R.string.enable_tracking_label)
-                Locus.stopLocationUpdates()
+//                Locus.stopLocationUpdates()
+                stopLocationUpdates()
             } else {
                 locationEnabled = true;
                 binding.switchButton.text = getString(R.string.disable_tracking_label)
-                Locus.startLocationUpdates(this) { result ->
-                    result.location?.let {
-                        binding.editTextTextMultiLine.setText(
-                            generateLocationOutput(it)
-                        )
-                    }
-                    result.error?.let { /* Received error! */ }
-                }
+//                Locus.startLocationUpdates(this) { result ->
+//                    result.location?.let {
+//                        binding.editTextTextMultiLine.setText(
+//                            generateLocationOutput(it)
+//                        )
+//                    }
+//                    result.error?.let { /* Received error! */ }
+//                }
+                startLocationUpdates()
             }
             // Code here executes on main thread after user presses button
         }
     }
 
-    private fun generateLocationOutput(it: Location): String {
-        return "Latitude: ${it.latitude} \r\n" +
-                "Longtitude: ${it.longitude} \r\n" +
-                "Accuracy: ${it.accuracy} \r\n" +
-                "Speed: ${it.speed} \r\n" +
-                "Time: ${getDateTime(it.time)} (${it.time}) \r\n" +
-                "Altitude: ${it.altitude} \r\n" +
-                "Bearing: ${it.bearing} \r\n" +
-                "Mock: ${it.isFromMockProvider} \r\n" +
-                "Extras: ${it.extras} \r\n" +
-                "Provider: ${it.provider} \r\n"
+    private fun setLocationOutput(it: Location) {
+        binding.latValue.text = it.latitude.toString()
+        binding.lonValue.text = it.longitude.toString()
+        binding.accValue.text = it.accuracy.toString()
+        binding.speedValue.text = it.speed.toString()
+        binding.timeValue.setText(getDateTime(it.time) + " ("+ it.time + ")")
+        binding.altValue.text = it.altitude.toString()
+        binding.bearValue.text = it.bearing.toString()
+        binding.mockValue.text = it.isFromMockProvider.toString()
+        binding.srcValue.text = it.provider.toString()
+        binding.editTextTextMultiLine.setText(it.extras.toString())
     }
 
     private fun getDateTime(milliseconds: Long): String {
@@ -88,6 +107,111 @@ class MainActivity : AppCompatActivity() {
         val cal = Calendar.getInstance()
         cal.timeInMillis = milliseconds
         return formatter.format(cal.time)
+    }
+
+    /**
+     * call this method in onCreate
+     * onLocationResult call when location is changed
+     */
+    private fun getLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+//        locationRequest.smallestDisplacement = 0 // 170 m = 0.1 mile
+        locationRequest.priority =
+            LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+
+                if (locationResult.locations.isNotEmpty()) {
+                    // get latest location
+                    val location =
+                        locationResult.lastLocation
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    setLocationOutput(location)
+                }
+            }
+        }
+    }
+
+    //start location updates
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    // stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    // stop receiving location update when activity not visible/foreground
+    override fun onPause() {
+        super.onPause()
+        if (locationEnabled) {
+            stopLocationUpdates()
+        }
+    }
+
+    // start receiving location update when activity  visible/foreground
+    override fun onResume() {
+        super.onResume()
+        if (locationEnabled) {
+            startLocationUpdates()
+        }
+    }
+
+    fun getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location->
+                if (location != null) {
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    setLocationOutput(location)
+                }
+
+            }
+
     }
 
 }
