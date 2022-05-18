52e610bb47cb0ca83623da5a98568a075389934c
alex <alkempled@gmail.com>
2022-05-18 23:44:52 +0300

    vibrator :D

--- app/src/main/java/com/alkempl/rlr/utils/CoreUtils.kt
+++ app/src/main/java/com/alkempl/rlr/utils/CoreUtils.kt
@@ -2,12 +2,19 @@ package com.alkempl.rlr.utils
 
 import android.Manifest
 import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
 import android.content.pm.PackageManager
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import java.text.DateFormat
 import java.util.*
+
+const val TAG = "Utils"
 
 class Utils {
 
@@ -22,6 +29,26 @@ fun getDateTime(milliseconds: Long): String {
     val cal = Calendar.getInstance()
     cal.timeInMillis = milliseconds
     return getDateTime(cal.time)
+}
+
+/*
+Vibrates with pattern
+e.g. val pattern = longArrayOf(0, 200, 100, 300)
+or takes second value to use
+ */
+fun vibrate(context: Context, pattern: LongArray){
+    if (pattern.size < 2){
+        Log.e(TAG, "vibrate: pattern less than 2")
+        return
+    }
+
+    val vib = context.getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        vib.vibrate(VibrationEffect.createWaveform(pattern, -1))
+    }else{
+        vib.vibrate(pattern[1])
+    }
 }
 
 
